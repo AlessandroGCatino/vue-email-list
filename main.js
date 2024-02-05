2740580cc2684016ce56a568f34b6bde4b62be15
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-02-05 16:59:36 +0100

    conclusione esercizio

--- main.js
+++ main.js
@@ -4,13 +4,28 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            generatedMails : [],
         }
     },
     created() {
         
     },
     methods: {
+        generaListaMail(){
+
+            this.generatedMails = []
+            for (let i=0; i<10; i++){
+
+
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then( ( result ) => {
+                    this.generatedMails.push(result.data.response)
+                    console.log(result.data.response)
+                }
+                )
+            }
+            console.log(this.generatedMails)
+        }
         
     }
 
