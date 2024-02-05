ae69699597afbf75b71599e596d6314aa5c46626
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-02-05 16:41:00 +0100

    inizializzazione e link file

---
+++ main.js
@@ -0,0 +1,20 @@
+const { createApp } = Vue
+
+
+createApp({
+    data() {
+        return {
+
+        }
+    },
+    created() {
+        
+    },
+    methods: {
+        
+    }
+
+}).mount('#app')
+
+/* API
+https://flynn.boolean.careers/exercises/api/random/mail */
